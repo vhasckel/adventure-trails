52ff63cac008f622502856c97cac7b524af4330c
vanessa hasckel <vanessahasckel00@hotmail.com>
2024-04-06 00:11:28 -0300

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Content from "./components/Content";
+import Footer from "./components/Footer";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Hero />
       <Content />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import styles from "./styles.module.css";
+
+function Footer() {
+  return (
+    <footer>
+      <p>Adventure Trails FD</p>
+    </footer>
+  );
+}
+
+export default Footer;
