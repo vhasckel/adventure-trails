aca91970d8cf59ae5318207bcadf98450406efed
vanessa hasckel <vanessahasckel00@hotmail.com>
2024-04-14 17:07:54 -0300

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
-import HomePage from "./pages/Home";
 import Footer from "./components/Footer";
 // import { UsersContextProvider } from "/context/usersContext";
 
