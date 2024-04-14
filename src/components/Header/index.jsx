ab81f642f6c60017a0b69439aacd627de189d888
vanessa hasckel <vanessahasckel00@hotmail.com>
2024-04-14 16:08:55 -0300

    header fixes

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,27 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { CiMountain1, CiMenuKebab } from "react-icons/ci";
 
 import styles from "./styles.module.css";
 
 function Header() {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 0;
+      setScrolled(isScrolled);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <header>
+    <header className={scrolled ? styles.scrolled : ""}>
       <div className={styles.nav}>
         <div className={styles.logo}>
           <Link to="/">
@@ -15,9 +31,11 @@ function Header() {
 
         <div className={styles.links}>
           <CiMenuKebab className={styles.smallScreen} size={32} />
+
           <Link to="/explorar-trilhas">
             <p className={styles.largeScreen}>Explorar trilhas</p>
           </Link>
+
           <Link to="/cadastrar-trilha">
             <p className={styles.largeScreen}>Cadastrar trilhas</p>
           </Link>
